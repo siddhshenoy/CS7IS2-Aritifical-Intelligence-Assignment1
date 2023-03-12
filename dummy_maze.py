02b77ed9a96ffd2d1d823c44b060c35272bc57bf
Siddharth Shenoy <shenoys@tcd.ie>
2023-03-12 16:38:58 +0000

    Removed unwanted python files:

--- dummy_maze.py
+++
@@ -1,7 +0,0 @@
-from pyamaze import maze,COLOR
-
-if __name__ == "__main__":
-    print("Starting..")
-    maze = maze(5, 5)
-    maze.CreateMaze(1,5,loopPercent=100,theme=COLOR.dark)
-    maze.run()
